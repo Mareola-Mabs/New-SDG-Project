626be5a28754a2c751e86658ed1e94c838dbe0b9
Ibukunola <bulun1001@gmail.com>
2024-11-28 12:00:12 +0100

    feat(form): add auto generate text to the contact form
    the auto generte text was added to aid users who wish to join the organisation whenever they click any of the join-us links
    
    this is a latter commit, the javascript file was not stagged in the previous commit

---
+++ js/join.js
@@ -0,0 +1,7 @@
+let join_link = document.querySelectorAll('.join__link')
+let message_box = document.getElementById('message')
+
+join_link.forEach(elem=>elem.addEventListener('click',()=>{
+    message_box.value = "Hi, i'm <Input Your Name>. I've read about your organisation, and would like to become a <Input Text>"
+}))
+
