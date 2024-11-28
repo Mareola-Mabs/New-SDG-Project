a72cb17595863c396a07a8e8490161d270e43d97
Ibukunola <bulun1001@gmail.com>
2024-11-28 15:03:22 +0100

    perf(javascript): merge join and intersection javascript files to one

--- js/intersection.js
+++ js/intersection.js
@@ -31,6 +31,26 @@ about_link.forEach(item=>item.addEventListener('click',()=>{
 }));
 
 
+let join_link = document.querySelectorAll('.join__link')
+let message_box = document.getElementById('message')
+
+join_link.forEach(elem=>elem.addEventListener('click',()=>{
+    message_box.value = "Hi, i'm <Input Your Name>. I've read about your organisation, and would like to become a <Input Text>"
+
+    
+
+    brands_that.forEach(item=>{
+        item.classList.add("move")
+        item.style.transition="0s"
+        
+    })
+
+
+
+}))
+
+
+
 
 // about.forEach(
 //     abouts =>{
